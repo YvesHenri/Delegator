d526fb29c018fed6e5beb4e1029e36f434118537
YvesHenri <yveshenricalaci@hotmail.com>
2017-02-02 11:07:59 -0300

    Added macros header for macro definitions

--- Tests/Sources/Main.cpp
+++ Tests/Sources/Main.cpp
@@ -1,12 +1,7 @@
+#include "..\Includes\Macros.h"
 #include "..\Includes\DummyClass.h"
 #include "..\Includes\LoggableDelegate.h"
 #include "..\Includes\Functor\LoggableFunctor.h"
-
-#define COMPARE(d1, d2) compare(#d1, #d2, d1, d2)
-void compare(char* d1n, char* d2n, LoggableDelegate<void(void)>& d1, LoggableDelegate<void(void)>& d2)
-{
-	printf("%s == %s: %s \n", d1n, d2n, d1 == d2 ? "TRUE!" : "false");
-}
 
 void runResetsTest()
 {
